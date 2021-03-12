278558fa04baad3d99662520cbdd6f2f5be1e866
KyleSimmonsC44 <darrensimmons92@gmail.com>
2021-03-11 18:38:56 -0600

    all create functions for the api have been created(lol) and tested

--- server/TheHopeAppapi/views/posts.py
+++ server/TheHopeAppapi/views/posts.py
@@ -5,7 +5,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from TheHopeAppapi.models import Post, category
+from TheHopeAppapi.models import Post, Category
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -41,3 +41,18 @@ class Posts(ViewSet):
             return Response(serializer.data)
         except Post.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+
+        post=Post()
+        post.category = Category.objects.get(pk=request.data["categoryId"])
+        post.user = request.auth.user
+        post.content = request.data['content']
+
+        try:
+            post.save()
+            serializer = PostSerializer(post, context={'request': request})
+            return Response(serializer.data)
+        
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
